e2b4983409205c1da9a8a71c949da0ad48b0b75b
Nao Takegawa <nao.takegawa316@gmail.com>
2018-04-16 07:12:34 +0900

    initial commit

---
+++ src/Content.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Header from './layouts/Header.js';
+
+class Content extends Component {
+  render() {
+    return (
+      <div>
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default Content;
